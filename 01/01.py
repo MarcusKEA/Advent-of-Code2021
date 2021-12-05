a8a458da55660a62055e1a192b684071eceb49ac
BuildTools <unconfigured@null.spigotmc.org>
2021-12-06 00:26:27 +0100

    02 Using sum()

--- 01/01.py
+++ 01/01.py
@@ -4,7 +4,8 @@ def problem1(l):
     return [int(l[i]) > int(l[i-1]) for i in range(1, len(l))].count(True)
 
 def problem2(l):
-    lst = [(int(l[i-2]) + int(l[i-1]) + int(l[i])) for i in range(2, len(l))]
+    l = [int(i) for i in l]
+    lst = [sum(l[i-2:i+1]) for i in range(2, len(l))]
     return problem1(lst)
 
 if __name__ == "__main__":
