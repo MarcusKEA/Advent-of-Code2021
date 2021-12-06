ccc6079bc75f2b2338f04702118389ef03cc2bb2
BuildTools <unconfigured@null.spigotmc.org>
2021-12-06 21:15:09 +0100

    3 Cleared First Half

--- 03/03.py
+++ 03/03.py
@@ -1,8 +1,36 @@
 import io
 
+def binatodeci(binary):
+    return sum(val*(2**idx) for idx, val in enumerate(reversed(binary)))
+
 def problem1(l:list):
+    item = dict()
+    gammaRate = []
+    epsilonRate = []
+
+    for i in range(12):
+        thing = {i : []}
+        item.update(thing)
+    for i in l:
+        for j in range(len(i)):
+            item[j].append(i[j])
     
-    return
+    for i in range(len(item)):
+        item[i] = [int(i) for i in item[i]]
+        ones = item[i].count(True)
+        zeroes = item[i].count(False)
+        
+        if ones > zeroes:
+            gammaRate.append(1)
+            epsilonRate.append(0)
+        else:
+            gammaRate.append(0)
+            epsilonRate.append(1)
+    
+    gammaRate = binatodeci(gammaRate)
+    epsilonRate = binatodeci(epsilonRate)
+
+    return gammaRate * epsilonRate
 
 def problem2(l):
     return
